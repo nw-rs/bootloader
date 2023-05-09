27ab73ec7e8ac2a1927b43c378eab7d0ee9f2d09
willemml <willem@leit.so>
2023-05-09 12:15:10 -0700

    remove cas dependency

--- src/main.rs
+++ src/main.rs
@@ -4,81 +4,58 @@
 
 extern crate cortex_m_rt as rt;
 
+use cortex_m_semihosting::hprintln;
 use rt::entry;
 
-use cortex_m_semihosting::hprintln;
-
-use core::{panic::PanicInfo, slice};
+use core::panic::PanicInfo;
 
 use nw_board_support::*;
-
 use nw_board_support::hal;
-use nw_board_support::hal::interrupt;
-
-use usb_device::device::{UsbDeviceBuilder, UsbVidPid};
-use usbd_dfu::DFUClass;
-
-use dfu::QspiDfu;
-
-mod dfu;
 
 #[inline(never)]
 #[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    hprintln!("{:?}", info);
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
-}
-
-#[interrupt]
-fn QUADSPI() {
-    get_led().blue();
-
-    unsafe {
-        let qspi = &(*hal::pac::QUADSPI::ptr());
-        qspi.cr.modify(|_, w| w.ftie().clear_bit());
-    }
 }
 
 #[entry]
 fn main() -> ! {
+    hprintln!("bootloader entry");
     let dp = unsafe { hal::pac::Peripherals::steal() };
 
-    static mut EP_MEMORY: [u32; 1024] = [0; 1024];
+    init_mpu();
 
-    let clocks = init_clocks(dp.RCC);
+    let _clocks = init_clocks(dp.RCC);
 
-    let usb_bus = get_usb_bus_allocator(&clocks, unsafe { &mut EP_MEMORY });
+    cortex_m::asm::dmb();
 
-    let mut display = get_display(&clocks);
-
+    hprintln!("init external flash");
     external_flash::init();
     external_flash::set_memory_mapped();
+    hprintln!("memory mapped mode enabled");
 
-    let test = unsafe { slice::from_raw_parts(0x90000000u32 as *const u32, 32) };
+    bootloader_mpu_init();
+    hprintln!("mpu configured for boot");
 
-    hprintln!("{:08x?}\n", test);
+    hprintln!("booting");
+    unsafe { cortex_m::asm::bootload(0x90000000u32 as *const u32) }
+}
 
-    let test2 = unsafe { slice::from_raw_parts(0x90000010u32 as *const u32, 16) };
+fn bootloader_mpu_init() {
+    cortex_m::asm::dmb();
 
-    hprintln!("{:08x?}\n", test2);
+    unsafe {
+        let mpu = &*cortex_m::peripheral::MPU::PTR;
 
-    external_flash::unset_memory_mapped();
+        mpu.ctrl.write(0);
 
-    let dfu_mem = QspiDfu::new();
+        mpu.rnr.write(7);
+        mpu.rbar.write(0x9000_0000);
+        mpu.rasr.write(1);
 
-    let mut dfu = DFUClass::new(&usb_bus, dfu_mem);
-
-    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x0483, 0xdf11))
-        .manufacturer("Numworks")
-        .product("RustWorks Bootloader")
-        .serial_number("TEST")
-        .device_class(0x02)
-        .build();
-
-    display.write_top("DFU interface enabled, write to 0x900000000 for external flash.");
-    display.draw_all();
-
-    loop {
-        usb_dev.poll(&mut [&mut dfu]);
+        mpu.ctrl.write(1);
     }
+
+    cortex_m::asm::dsb();
+    cortex_m::asm::isb();
 }
